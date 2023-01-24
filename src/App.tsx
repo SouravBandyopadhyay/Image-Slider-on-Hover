a0173e20d65cba28a9233f0991560c7715ad7ff6
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="card">
+          <img
+            src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+            alt="imageLink_broken"
+          />
+        </div>
+        <div className="card">
+          <img
+            src="https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__480.jpg"
+            alt="imageLink_broken"
+          />
+        </div>
+        <div className="card">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171__480.jpg"
+            alt="imageLink_broken"
+          />
+        </div>
+        <div className="card">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/02/13/12/26/aurora-1197753__480.jpg"
+            alt="imageLink_broken"
+          />
+        </div>
+        <div className="card">
+          <img
+            src="https://cdn.pixabay.com/photo/2013/04/04/12/34/mountains-100367__480.jpg"
+            alt="imageLink_broken"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
